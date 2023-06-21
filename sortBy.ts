963153e72d82eb09c226b6afc8cf80f0f07c4a7a
Josh <josh.dronsfield@gmail.com>
2023-06-21 15:02:01 +0100

    array sortBy

--- sortBy.ts
+++ sortBy.ts
@@ -1,60 +1,75 @@
 function isEmpty(value: any) {
-  return Boolean(!value && value !== false)
+  return Boolean(!value && value !== false && value !== 0);
 }
 
+type Prop<T> = keyof T | ((input: T) => any);
+
 function compareFactory<T>(opts: {
-  prop: keyof T
-  desc?: boolean
-  valueTransform: (x: T[keyof T]) => any
-  secondaryProp?: keyof T
+  prop?: Prop<T> | Array<Prop<T>>;
+  desc?: boolean;
+  valueTransform: (x: any) => any;
 }): (left: T, right: T) => number {
-  const { prop, desc, valueTransform, secondaryProp } = opts
-  const directionTransform = (n: number) => (desc ? -1 : 1) * n
-  const secondaryCompare = secondaryProp
-    ? compareFactory({ prop: secondaryProp, valueTransform, desc: false })
-    : () => 0
+  const { prop: _prop, desc, valueTransform } = opts;
+
+  const isPropArray = Array.isArray(_prop);
+  const prop = isPropArray ? _prop[0] : _prop;
+
+  const directionTransform = (n: number) => (desc ? -1 : 1) * n;
+  const secondaryCompare =
+    isPropArray && _prop.length > 1
+      ? compareFactory({ prop: _prop.slice(1), valueTransform, desc: false })
+      : () => 0;
 
   return (left: T, right: T) => {
-    let order = 0
-    if (isEmpty(left[prop]) && isEmpty(right[prop])) {
-      order = secondaryCompare(left, right)
-    } else if (isEmpty(left[prop])) {
-      order = 1
-    } else if (isEmpty(right[prop])) {
-      order = -1
+    const leftProp = prop
+      ? typeof prop === "function"
+        ? prop(left)
+        : left[prop]
+      : left;
+    const rightProp = prop
+      ? typeof prop === "function"
+        ? prop(right)
+        : right[prop]
+      : right;
+    let order = 0;
+    if (isEmpty(leftProp) && isEmpty(rightProp)) {
+      order = secondaryCompare(left, right);
+    } else if (isEmpty(leftProp)) {
+      order = 1;
+    } else if (isEmpty(rightProp)) {
+      order = -1;
     } else {
-      const l = valueTransform(left[prop])
-      const r = valueTransform(right[prop])
+      const l = valueTransform(leftProp);
+      const r = valueTransform(rightProp);
       if (l > r) {
-        order = directionTransform(1)
+        order = directionTransform(1);
       } else if (l < r) {
-        order = directionTransform(-1)
+        order = directionTransform(-1);
       } else if (l === r) {
-        order = secondaryCompare(left, right)
+        order = secondaryCompare(left, right);
       }
     }
-    return order
-  }
+    return order;
+  };
 }
 
 export function sortBy<T>(
   input: T[],
-  prop: keyof T,
+  prop?: Prop<T> | Array<Prop<T>>,
   desc?: boolean,
   opts?: {
-    castLower?: boolean
-    secondaryProp?: keyof T
-    valueTransform?: (x: T[keyof T]) => any
+    castLower?: boolean;
+    valueTransform?: (x: any) => any;
   }
 ) {
-  const { castLower, secondaryProp, valueTransform: valueTransformProp } =
-    opts || {}
+  const { castLower, valueTransform: valueTransformProp } = opts || {};
   const valueTransform =
-    valueTransformProp || castLower
+    valueTransformProp ||
+    (castLower
       ? (x: T[keyof T]) => (typeof x === "string" ? x.toLowerCase() : x)
-      : (x: T[keyof T]) => x
+      : (x: T[keyof T]) => x);
 
-  const compare = compareFactory({ prop, desc, valueTransform, secondaryProp })
+  const compare = compareFactory({ prop, desc, valueTransform });
 
-  return [...input].sort(compare)
+  return [...input].sort(compare);
 }
